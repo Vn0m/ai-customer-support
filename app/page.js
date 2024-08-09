16bb191a5c2ba910855a994823b57093b2266e51
Cindy Tran <ctran@WI-BMS-MBA25.local>
2024-08-09 17:04:24 -0400

    basic structure

--- app/page.js
+++ app/page.js
@@ -1,8 +1,68 @@
+'use client'
+import {Box, Typography, Stack} from '@mui/material'
 import Image from "next/image";
-import styles from "./page.module.css";
+import { useState } from 'react'
 
 export default function Home() {
+  const [messages, setMessages] = useState([
+    {role: 'assistant', 
+    content: 'Hi, how can I assist you today?'
+    }
+  ])
+  const [message, setMessage] = useState('')
+
+  const sendMessage = async() => {
+    setMessage('')
+  }
+
   return (
-    <div>test</div>
-  );
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack
+      direction="column"
+      width="600px"
+      height="700px"
+      border="1px solid black"
+      p={2}
+      spacing={3}
+      >
+        <Stack 
+          direction="column"
+          spacing={2}
+          flexGrow={1}
+          overflow="auto"
+          maxHeight="100px"
+        >
+          {
+            messages.map((messages, index) => {
+              <Box 
+                key={index}
+                display="flex"
+                justifyContent={
+                  message.role === 'assistant' ? 'flex-start' : 'flex-end'
+                }
+              >
+                <Box
+                  bgcolor={
+                    message.role === 'assistant'
+                    ? 'primary.main'
+                    : 'secondary.main'
+                  }
+                  color="white"
+                  borderRadius={16}
+                  p={3}
+                ></Box>
+              </Box>
+            })
+          }
+        </Stack>
+      </Stack>
+    </Box>
+  )
 }
